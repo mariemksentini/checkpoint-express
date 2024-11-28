7d3d7c2078c67008c88fe7a9797733e7da1f8ec8
mariemksentini <maryemksentini@gmail.com>
2024-11-28 22:19:17 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+
+
+function checkdate(req,res,next){
+    const date = new Date();
+    const day = date.getDay()	
+    const hour = date.getHours()
+//&& hour >=9 && hour <=17
+    if (day >=1 && day <=5 ){
+        next()
+
+    }else{
+        res.send('<h1>the page is available only in workdays</h1>')
+    }
+}
+
+
+app.use(express.static('public'))
+
+app.use(checkdate)
+
+app.get('/style.css',(req,res)=>{
+    res.sendFile(__dirname+"/public/style.css")
+})
+
+app.get('/home', (req,res,next)=>{
+    res.sendFile(__dirname+'/home.html')
+})
+
+app.get('/contact', (req,res,next)=>{
+    res.sendFile(__dirname+'/contact.html')
+})
+
+app.get('/services', (req,res,next)=>{
+    res.sendFile(__dirname+'/services.html')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+})
