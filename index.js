e5f10f9acd13cd6a699b6829b627530025455eb2
mariemksentini <maryemksentini@gmail.com>
2024-11-29 10:06:48 +0100

    second

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function checkdate(req,res,next){
     const date = new Date();
     const day = date.getDay()	
     const hour = date.getHours()
-//&& hour >=9 && hour <=17
-    if (day >=1 && day <=5 ){
+//
+    if (day >=1 && day <=5 && hour >=9 && hour <=17){
         next()
 
     }else{
